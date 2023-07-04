f0167a1058902048476455d354e97ffefa2156ad
Miguel Angel Perez Garcia <miguelangel.perez.dev@gmail.com>
2023-07-04 18:38:00 +0100

    test: add edge case to check highest number function return one number from an array with identical values

--- __test__/highest-number/highest-number.spec.ts
+++ __test__/highest-number/highest-number.spec.ts
@@ -10,4 +10,9 @@ describe('getHighestNumber', () => {
         const numbers: Array<number> = [1, 5, 3]
         expect(highestNumber(numbers)).toEqual(5)
     });
+
+    it('should return one number from an array with identical values', function () {
+        const numbers: Array<number> = [5, 5, 5]
+        expect(highestNumber(numbers)).toEqual(5)
+    });
 })
